e2b289ab9ebba259efed13428d087fd77cb58b96
Denis Popkov <pkvdenis@gmail.com>
2022-04-07 18:46:46 +0300

    Add Spring Boot empty project.

---
+++ src/test/java/learn/springws/restfulws/RestfulwsApplicationTests.java
@@ -0,0 +1,13 @@
+package learn.springws.restfulws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfulwsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
