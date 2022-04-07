19e0272793c5ff4cd471c4b3774ab28a6f7e93a1
Denis Popkov <pkvdenis@gmail.com>
2022-04-08 01:16:01 +0300

    Add Spring Security, encrypt User password.

--- src/main/java/learn/springws/restfulws/service/impl/UserServiceImpl.java
+++ src/main/java/learn/springws/restfulws/service/impl/UserServiceImpl.java
@@ -6,6 +6,7 @@ import learn.springws.restfulws.service.UserService;
 import learn.springws.restfulws.shared.Utils;
 import learn.springws.restfulws.shared.dto.UserDto;
 import org.springframework.beans.BeanUtils;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -15,10 +16,12 @@ public class UserServiceImpl implements UserService {
 
     private final UserRepository userRepository;
     private final Utils utils;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
 
-    public UserServiceImpl(UserRepository userRepository, Utils utils) {
+    public UserServiceImpl(UserRepository userRepository, Utils utils, BCryptPasswordEncoder bCryptPasswordEncoder) {
         this.userRepository = userRepository;
         this.utils = utils;
+        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
     }
 
     @Override
@@ -30,7 +33,7 @@ public class UserServiceImpl implements UserService {
         BeanUtils.copyProperties(dto, entity);
         String publicUserId = utils.generateUserId(PUBLIC_USER_ID_LENGTH);
         entity.setUserId(publicUserId);
-        entity.setEncryptedPassword("test");
+        entity.setEncryptedPassword(bCryptPasswordEncoder.encode(dto.getPassword()));
         UserEntity storedUser = userRepository.save(entity);
         UserDto result = new UserDto();
         BeanUtils.copyProperties(storedUser, result);
