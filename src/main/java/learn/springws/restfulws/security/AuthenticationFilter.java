29da7237e4cd671fa1c7895641a9856f9baeab7d
Denis Popkov <pkvdenis@gmail.com>
2022-04-08 16:05:34 +0300

    Sign-in: implement AuthenticationFilter,
    to test it with Postman you can use the default Spring endpoint http://localhost:8080/login.

---
+++ src/main/java/learn/springws/restfulws/security/AuthenticationFilter.java
@@ -0,0 +1,58 @@
+package learn.springws.restfulws.security;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import learn.springws.restfulws.rest.model.request.UserLoginRequestModel;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Collections;
+import java.util.Date;
+
+public class AuthenticationFilter extends UsernamePasswordAuthenticationFilter {
+
+    private final AuthenticationManager authenticationManager;
+
+    public AuthenticationFilter(AuthenticationManager authenticationManager) {
+        this.authenticationManager = authenticationManager;
+    }
+
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response)
+            throws AuthenticationException {
+        try {
+            UserLoginRequestModel credentials = new ObjectMapper()
+                    .readValue(request.getInputStream(), UserLoginRequestModel.class);
+            return authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(
+                            credentials.getEmail(),
+                            credentials.getPassword(),
+                            Collections.emptyList()
+                    )
+            );
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response,
+                                            FilterChain chain, Authentication authResult) {
+        String userName = ((User) authResult.getPrincipal()).getUsername();
+        String token = Jwts.builder()
+                .setSubject(userName)
+                .setExpiration(new Date(System.currentTimeMillis() + SecurityConstants.EXPIRATION_TIME))
+                .signWith(SignatureAlgorithm.HS512, SecurityConstants.TOKEN_SECRET)
+                .compact();
+        response.addHeader(SecurityConstants.TOKEN_HEADER, SecurityConstants.TOKEN_PREFIX + token);
+    }
+}
--- src/main/java/learn/springws/restfulws/security/SecurityConstants.java
+++ src/main/java/learn/springws/restfulws/security/SecurityConstants.java
@@ -2,5 +2,9 @@ package learn.springws.restfulws.security;
 
 public class SecurityConstants {
 
+    public static final long EXPIRATION_TIME = 10 * 24 * 60 * 60 * 1000;    // 10 days in ms
+    public static final String TOKEN_PREFIX = "Bearer ";
+    public static final String TOKEN_HEADER = "Authorization";
+    public static final String TOKEN_SECRET = "a7slxo29oxnA7b";
     public static final String SIGN_UP_URL = "/users";
 }
