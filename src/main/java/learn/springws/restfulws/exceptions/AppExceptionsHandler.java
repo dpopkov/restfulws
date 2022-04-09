97f20a226846689e30eda0a95c1c499e734c608c
Denis Popkov <pkvdenis@gmail.com>
2022-04-09 17:38:17 +0300

    Exceptions: add AppExceptionsHandler to handle specific exceptions.

---
+++ src/main/java/learn/springws/restfulws/exceptions/AppExceptionsHandler.java
@@ -0,0 +1,20 @@
+package learn.springws.restfulws.exceptions;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+
+@Slf4j
+@ControllerAdvice
+public class AppExceptionsHandler {
+
+    @ExceptionHandler(value = {UserServiceException.class})
+    public ResponseEntity<Object> handleUserServiceException(UserServiceException ex, WebRequest request) {
+        log.trace("handleUserServiceException(..) : message = {}", ex.getMessage());
+        return new ResponseEntity<>(ex.getMessage(), new HttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+}
