84a0f7cee81374da46c207b1796357f2e876ff39
Denis Popkov <pkvdenis@gmail.com>
2022-04-10 20:27:08 +0300

    User one-to-many Addresses: add AddressRequestModel.

---
+++ src/main/java/learn/springws/restfulws/rest/model/request/AddressRequestModel.java
@@ -0,0 +1,12 @@
+package learn.springws.restfulws.rest.model.request;
+
+import lombok.Data;
+
+@Data
+public class AddressRequestModel {
+    private String city;
+    private String country;
+    private String streetName;
+    private String postalCode;
+    private String type;
+}
--- src/main/java/learn/springws/restfulws/rest/model/request/UserDetailsRequestModel.java
+++ src/main/java/learn/springws/restfulws/rest/model/request/UserDetailsRequestModel.java
@@ -2,10 +2,13 @@ package learn.springws.restfulws.rest.model.request;
 
 import lombok.Data;
 
+import java.util.List;
+
 @Data
 public class UserDetailsRequestModel {
     private String firstName;
     private String lastName;
     private String email;
     private String password;
+    private List<AddressRequestModel> addresses;
 }
